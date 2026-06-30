749e16e20768dbe4928b7f3347960a0a7074fc0b
Marc-Dip-Faye <marcfaye457@gmail.com>
2026-06-30 23:00:02 +0000

    Enregistrement de la nouvelle categorie

--- index.php
+++ index.php
@@ -31,3 +31,43 @@
             echo $categories[$index]["nom"]."\n";
         }
     }
+
+    // 3 Enregistrer une nouvelle categorie
+        
+    do{
+        $codeValide = true;
+        $code = readline("Saisir le code : ");
+        if(empty($code) == false){
+            for($index = 0; $index < count($categories); $index++){
+                if($categories[$index]["code"] == $code){
+                    $codeValide = false;
+                    echo "Ce code existe déjà \n";
+                }
+            }
+        }else{
+            $codeValide = false;
+            echo "Ce champ est obligatoire !!\n";
+        }
+    }while(!$codeValide);
+   
+    do{
+        $nomValide = true;
+        $nom = readline("Saisir le nom : ");
+        if(empty($nom) == false){
+            for($index = 0; $index < count($categories); $index++){
+                if($categories[$index]["nom"] == $nom){
+                    $nomValide = false;
+                    echo "Ce nom existe déjà \n";
+                }
+            }
+        }else{
+            $nomValide = false;
+            echo "Ce champ est obligatoire !!\n";
+        }
+    }while(!$nomValide);
+
+    $categorie = [
+        "nom" => $nom,
+        "code" => $code,
+        "listeProduits" => []
+    ];

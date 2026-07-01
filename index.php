17e31fb219df1e8f51f98b7e04ea0bb9df012fbc
Marc-Dip-Faye <marcfaye457@gmail.com>
2026-07-01 00:46:01 +0000

    innitialisation du tableau de categorie

--- index.php
+++ index.php
@@ -1,2 +1,27 @@
 <?php
-    
+     $categories = [
+        [
+            "nom" => "Alimentaire",
+            "code" => "2465",
+            "listeProduits" => [
+                [
+                    "nom" => "Lait",
+                    "reference" => "A23L",
+                    "quantite" => 20,
+                    "prix" => 3200
+                ],
+                [
+                    "nom" => "Sucre",
+                    "reference" => "A4CS",
+                    "quantite" => 60,
+                    "prix" => 7000
+                ]
+            ],  
+        ],
+        [
+            "nom" => "Cosmetique",
+            "code" => "4190",
+            "listeProduits" => []
+        ]
+    ];
+

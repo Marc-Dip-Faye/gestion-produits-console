d01c66809b184da43ab6b96560c8f2ec402c6e37
Marc-Dip-Faye <marcfaye457@gmail.com>
2026-07-01 01:08:34 +0000

    Ajout caategorie en lui affectant produits

--- index.php
+++ index.php
@@ -70,3 +70,19 @@
         } while (!$valueIsValid);
         return $value;
     }
+
+    // 5
+
+    function enregistrerCategorie(): void{
+    global $categories;
+    $code = saisieChampObligatoireEtUnique($categories,"Entrez le code :", "champs obligatoire : ", "code");
+    $nom = saisieChampObligatoireEtUnique($categories,"Entrez le nom :", "champs obligatoire : ", "nom");
+
+    $categorie  =   [
+            "code" => $code,
+            "nom" => $nom,
+            "produits" => []
+         ];
+
+    $categories[] = $categorie;
+ }
